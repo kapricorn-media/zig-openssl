50df575f3bb1f7060298bc377675a5522b1fd1a4
Jose Rico <jmrico01@gmail.com>
2021-09-30 21:33:16 -0400

    support aarch64

--- build.zig
+++ build.zig
@@ -1,4 +1,33 @@
+const builtin = @import("builtin");
 const std = @import("std");
+
+pub fn targetToTripletString(target: std.zig.CrossTarget) ![]const u8
+{
+    if (target.os_tag) |os| {
+        if (os != .linux) {
+            return error.UnsupportedOs;
+        }
+    }
+    else {
+    }
+
+    if (target.abi) |abi| {
+        if (abi != .gnu) {
+            return error.UnsupportedAbi;
+        }
+    }
+    else {
+    }
+
+    const arch = if (target.cpu_arch) |a| a else builtin.cpu.arch;
+    switch (arch) {
+        .x86_64 => return "x86_64-linux-gnu",
+        .aarch64 => return "aarch64-linux-gnu",
+        else => return error.UnsupportedArch,
+    }
+
+    return error.UnsupportedTarget;
+}
 
 pub fn build(b: *std.build.Builder) void
 {
@@ -10,15 +39,20 @@ pub fn build(b: *std.build.Builder) void
     exe.linkLibC();
     exe.install();
 
-    addLib(exe, ".");
+    addLib(exe, target, ".") catch unreachable;
 }
 
-pub fn addLib(step: *std.build.LibExeObjStep, comptime dir: []const u8) void
+pub fn addLib(step: *std.build.LibExeObjStep, target: std.zig.CrossTarget, comptime dir: []const u8) !void
 {
-    step.addIncludeDir(dir ++ "include");
-    step.addIncludeDir(dir ++ "include/x86_64-linux-gnu");
-    const fullPath = "C:\\Users\\Jose Rico\\Documents\\Development\\zig-openssl";
-    step.addSystemIncludeDir(fullPath ++ "\\include");
-    step.addSystemIncludeDir(fullPath ++ "\\include\\x86_64-linux-gnu");
-    step.addLibPath(dir ++ "lib/x86_64-linux-gnu");
+    var buf: [2048]u8 = undefined;
+    const tripletString = try targetToTripletString(target);
+
+    step.addIncludeDir(dir ++ "/include");
+    const includePath = try std.fmt.bufPrint(buf[0..], "{s}/include/{s}", .{dir, tripletString});
+    step.addIncludeDir(includePath);
+
+    const libPath = try std.fmt.bufPrint(buf[0..], "{s}/lib/{s}", .{dir, tripletString});
+    step.addLibPath(libPath);
+    step.linkSystemLibrary("ssl");
+    step.linkSystemLibrary("crypto");
 }
